bed4acc38340eafab421c03877bd26e1abd7f952
river226 <river226@43e64c4e-4afe-d443-3390-869707027280>
2012-03-27 16:29:26 +0000

    update

--- branches/src/Player.java
+++ branches/src/Player.java
@@ -1,17 +1,73 @@
+/**
+ * Group 5 Robber Knights Game
+ *
+ * The Player Class:
+ *
+ * The Player Class holds and manages the players deck, hand and knights 
+ * The player has a color and an id that is a number
+ * test
+ */
 
-public class Player {
+class Player {
 
-	String name;
-	String color;
-	Hand hand;
+	private final int id;
+	private final String color;
+	private PlayerTile tiles;
+	private int remainingKnights;
 	
-	public Player(String name, String color){
-		this.name = name;
-		this.color = color;
+	// constructor, initialize variables
+	public Player(int num) {
+		id = num;
+		
+		if(id == 1)
+			color = "Red";
+		else if(id == 2)
+			color = "Blue";
+		else if(id == 3)
+			color = "Yellow";
+		else
+			color = "Green";
+		
+		tiles = new PlayerTile();
+		
+		// player starts out with 30 knights
+		remainingKnights = 30;
 	}
 	
+	// Get the players color 
+	public String getColor() {
+		return color;
+	}
 	
-	public String toString(){
-		return this.name + " " + this.color + " " + this.hand.displayHand();
+	// Get the Hand 
+	public Tile[] getHand() {
+		return tiles.showHand();
+	}
+	
+	// Get the number of knights left to play
+	public int remainingKnights() {
+		return remainingKnights;
+	}
+	
+	// Get the Score value of the Tile
+	public void playKnights(int count) {
+		remainingKnights -= count;
+	}
+	
+	// Get the a tile from the hand and add a new tile
+	public Tile makeMove(int location) {
+		Tile t = tiles.playTile(location);
+		tiles.draw();
+		return t;
+	}
+
+	// Make the first move by getting the the top set
+	public Tile[] firstMoveGet() {
+		return tiles.getSetA();
+	}
+	
+	// Restore the hand after making the first move
+	public void firstMoveSet(Tile t) {
+		tiles.addTile(t);
 	}
 }
