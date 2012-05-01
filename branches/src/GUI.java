55f528cf23713da7e168c7cf2ba320e959cd917c
river226 <river226@43e64c4e-4afe-d443-3390-869707027280>
2012-05-01 02:08:49 +0000

    GUI update

--- branches/src/GUI.java
+++ branches/src/GUI.java
@@ -1,24 +1,20 @@
-/**
- *
- * Group 5 Robber Knights Game
- *
- * Members: Jedidiah Johnson, Alex Sokol, Aaron Thrasher, Rebecca Rasmussen, Tony Dederich
- *
- * The GUI class:
- *
- * Implements the user facing portion of the program. It also fundtions as the driver for the entire game
- * and listens for all input from the user, interpreting the appropriate calls to make. 
- *
- */
-
 import java.awt.*;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 
 public class GUI implements ActionListener {
-	
-	// GUI Components
+
+	private int grid = 12;
+	//GUI Components
     private JFrame window = new JFrame("Robber Knights");
+    
     private JPanel board =  new JPanel();
     private JPanel playerStatus = new JPanel();
     private JDialog menu = new JDialog();
@@ -51,37 +47,51 @@ public class GUI implements ActionListener {
 	private JLabel playerThree = new JLabel();
 	private JLabel playerFour = new JLabel();
     private int numPlayers = 2;
-	private int grid = 12;
+    private RobberKnightsGame game;
     
-	// Creates GUI
-    public GUI() {
+    
+  //Creates GUI
+    public GUI() throws IOException
+    {
     	JMenuItem newAction = new JMenuItem("Start Game");
         JMenuItem exitAction = new JMenuItem("Exit");
         
         JMenuItem helpAction = new JMenuItem("Help");
         JMenuItem aboutAction = new JMenuItem("About");
         
-    	// Creates the main frame 
-    	window.setSize(800,600);                        
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
-        window.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.BOTH;
+    	//Creates the main frame 
+    	window.setSize(800,650);                        
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        
+        BufferedImage titleScreen = ImageIO.read(new File("TitleScreen.bmp"));
+        JLabel startScreen = new JLabel(new ImageIcon(titleScreen));
+        window.setContentPane(startScreen);
+     
+        board.setLocation(0, 0);
        
-        board.setSize(new Dimension(600,600));
+       
+        board.setSize(new Dimension(600, 600));
         board.setLayout(new GridLayout(grid,grid));
+		playerNameLabel.setBounds(5, 10, 50, 25);
+        
 
 		playerNameLabel.setText("Player: ");
+		playerNameField.setBounds(65, 10, 125, 25);
 		playerNameField.setText("Test 1");
+		knightsLabel.setBounds(5, 45, 115, 25);
 		knightsLabel.setText("Knights Remaining: ");
+		knightsField.setBounds(120, 45, 70, 25);
 		knightsField.setText("0");
+		tilesLabel.setBounds(5, 80, 100, 25);
 		tilesLabel.setText("Tiles Remaining: ");
+		tilesField.setBounds(105, 80, 85, 25);
 		tilesField.setText("0");
 		playTiles[0] = new JButton("Tile 1");
 		playTiles[1] = new JButton("Tile 2");
+		playerStatus.setLocation(600, 0);
 		
-		playerStatus.setSize(new Dimension(200,600));
-        playerStatus.setLayout(new GridLayout(8,2));
+		playerStatus.setSize(new Dimension(200, 600));
+		playerStatus.setLayout(null);
 		
 		playerStatus.add(playerNameLabel);
 		playerStatus.add(playerNameField);
@@ -91,13 +101,25 @@ public class GUI implements ActionListener {
 		playerStatus.add(tilesField);
 		playerStatus.add(playTiles[0]);
 		playerStatus.add(playTiles[1]);
+		
+		JLabel handLabel = new JLabel("Hand:");
+		handLabel.setBounds(80, 115, 40, 25);
+		playerStatus.add(handLabel);
+		
+		JButton tileOne = new JButton("Tile 1");
+		tileOne.setBounds(35, 150, 60, 60);
+		playerStatus.add(tileOne);
+		
+		JButton tileTwo = new JButton("Tiles 2");
+		tileTwo.setBounds(100, 150, 60, 60);
+		playerStatus.add(tileTwo);
+		back.setBounds(5, 220, 90, 25);
 		playerStatus.add(back);
+		endTurn.setBounds(100, 220, 90, 25);
 		playerStatus.add(endTurn);
-        c.weighty = 0.1;
-		c.weightx = 0.1;
-        window.add(board, c);
-        //c.weightx = .2;
-        window.add(playerStatus, c);
+        window.getContentPane().setLayout(null);
+      
+        
         window.setResizable(false);
         window.setJMenuBar(menuBar);
         
@@ -110,57 +132,61 @@ public class GUI implements ActionListener {
         helpMenu.add(helpAction);
         helpMenu.add(aboutAction);
         
-        // Creates the new game menu
+        JPanel namePane = new JPanel();
+    	JPanel radioPane = new JPanel();
+    	
+    	namePane.setLayout(new GridLayout(4,2));
+    	radioPane.setLayout(new GridLayout(3,1));
+    	
+    	menu.setSize(350,250);
+        menu.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        menu.setLayout(new GridLayout(2,1));
+        menu.setResizable(false);
+        
+        playerOne.setText("Player One: ");
+        playerTwo.setText("Player Two: ");
+        playerThree.setText("Player Three: ");
+        playerFour.setText("Player Four: ");
+        namePane.add(playerOne);
+        namePane.add(playerOneName);
+        namePane.add(playerTwo);
+        namePane.add(playerTwoName);
+        namePane.add(playerThree);
+        namePane.add(playerThreeName);
+        namePane.add(playerFour);
+        namePane.add(playerFourName);   
+        
+        group.add(twoPlayers);
+        group.add(threePlayers);
+        group.add(fourPlayers);
+      
+        twoPlayers.setEnabled(true);
+        
+        radioPane.add(twoPlayers);
+        radioPane.add(threePlayers);
+        radioPane.add(fourPlayers);
+        
+        menu.getContentPane().add(radioPane);
+        menu.getContentPane().add(namePane);
+        menu.getContentPane().add(newGame);
+        menu.getContentPane().add(close);
+        
+        twoPlayers.setSelected(true);
+        playerThreeName.disable();
+        playerThreeName.setBackground(null);
+        playerFourName.disable();
+        playerFourName.setBackground(null);
+        
+        //Creates the new game menu
         newAction.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
             	
-            	JPanel namePane = new JPanel();
-            	JPanel radioPane = new JPanel();
             	
-            	namePane.setLayout(new GridLayout(4,2));
-            	radioPane.setLayout(new GridLayout(3,1));
-            	
-            	menu.setSize(350,250);
-                menu.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-                menu.setLayout(new GridLayout(2,1));
-                menu.setResizable(false);
+      
                 
-                playerOne.setText("Player One: ");
-                playerTwo.setText("Player Two: ");
-                playerThree.setText("Player Three: ");
-                playerFour.setText("Player Four: ");
-                namePane.add(playerOne);
-                namePane.add(playerOneName);
-                namePane.add(playerTwo);
-                namePane.add(playerTwoName);
-                namePane.add(playerThree);
-                namePane.add(playerThreeName);
-                namePane.add(playerFour);
-                namePane.add(playerFourName);   
-                
-                group.add(twoPlayers);
-                group.add(threePlayers);
-                group.add(fourPlayers);
-                twoPlayers.setEnabled(true);
-                
-                radioPane.add(twoPlayers);
-                radioPane.add(threePlayers);
-                radioPane.add(fourPlayers);
-                
-                menu.add(radioPane);
-                menu.add(namePane);
-                menu.add(newGame);
-                menu.add(close);
-                
-                twoPlayers.setSelected(true);
-                playerThreeName.disable();
-                playerThreeName.setBackground(null);
-                playerFourName.disable();
-                playerFourName.setBackground(null);
-                //newGame.setEnabled(true);
-                
-				// Clears the board and hides the new game menu
-                twoPlayers.addActionListener(new ActionListener() {
+              
+                twoPlayers.addActionListener(new ActionListener() 
+                {
 					public void actionPerformed(ActionEvent arg0) {
 						playerThreeName.disable();
 						playerThreeName.setText(null);
@@ -172,7 +198,8 @@ public class GUI implements ActionListener {
 						
 					}	
                 });
-                threePlayers.addActionListener(new ActionListener() {
+                threePlayers.addActionListener(new ActionListener() 
+                {
 					public void actionPerformed(ActionEvent arg0) {
 						playerThreeName.enable();
 						playerThreeName.setBackground(Color.WHITE);
@@ -183,7 +210,8 @@ public class GUI implements ActionListener {
 						
 					}	
                 });
-                fourPlayers.addActionListener(new ActionListener() {
+                fourPlayers.addActionListener(new ActionListener() 
+                {
 					public void actionPerformed(ActionEvent arg0) {
 						playerThreeName.enable();
 						playerThreeName.setBackground(Color.WHITE);
@@ -193,22 +221,42 @@ public class GUI implements ActionListener {
 						
 					}	
                 });
-                newGame.addActionListener(new ActionListener() {
-                	public void actionPerformed(ActionEvent arg0) {
-                		reset();
+                //Clears the board and hides the new game menu
+                newGame.addActionListener(new ActionListener() 
+                {
+                	public void actionPerformed(ActionEvent arg0) 
+                	{
+                		window.getContentPane().add(board);
+                	     
+                        window.getContentPane().add(playerStatus);
+                		//reset();
+                		board.removeAll();
+                		board.repaint();
+                		playerStatus.repaint();
                 		playerNames[0] = playerOneName.getText();
                 		playerNames[1] = playerTwoName.getText();
                 		playerNames[2] = playerThreeName.getText();
                 		playerNames[3] = playerFourName.getText();
+                		game = new RobberKnightsGame(numPlayers, playerNames);
+                		grid = game.board.getMax() + 2;
+                		gridButtons = new JButton[grid][grid];
+                		board.setLayout(new GridLayout(grid,grid));
+                		setBoard();
+                		//window.setContentPane(board);
+                		board.validate();
                 		
                 		menu.setVisible(false);
+                		
                 	}
                 });
                 
-                // Hides the new game menu
-                close.addActionListener(new ActionListener() {
-                	public void actionPerformed(ActionEvent arg0) {
+                //Hides the new game menu
+                close.addActionListener(new ActionListener() 
+                {
+                	public void actionPerformed(ActionEvent arg0) 
+                	{
                 		menu.setVisible(false);
+                	
                 	}
                 });
                 
@@ -216,15 +264,28 @@ public class GUI implements ActionListener {
             }
         });
         
-        // Closes program
-        exitAction.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent arg0) {
+        //Closes program
+        exitAction.addActionListener(new ActionListener() 
+        {
+        	public void actionPerformed(ActionEvent arg0)
+        	{
         		System.exit(0);
         	}
         });
-        // Put the squares on the main frame
-        for(int x = 0; x < grid; x++) {
-        	for(int y = 0; y < grid; y++) {
+        
+        //setBoard();			
+        
+        
+        
+        window.setVisible(true);     
+    }
+    public void setBoard()
+    {
+    	//Put the squares on the main frame
+        for(int x = 0; x < grid; x++)
+        {
+        	for(int y = 0; y < grid; y++)
+        	{
         		gridButtons[x][y] = new JButton();                    
         		board.add(gridButtons[x][y]);                        
         		gridButtons[x][y].addActionListener(this);            
@@ -233,14 +294,16 @@ public class GUI implements ActionListener {
         		gridButtons[x][y].setEnabled(false);
         	}
         }
-        
-        window.setVisible(true);     
     }
+
     
-	// Resets the board
-    public void reset() {
-    	for (int x = 0; x < grid; x++) {
-    		for (int y = 0; y < grid; y++) {
+  //Resets the board
+    public void reset() 
+    {
+    	for (int x = 0; x < grid; x++)
+    	{
+    		for (int y = 0; y < grid; y++)
+    		{
     			gridButtons[x][y].setText("");
     			gridButtons[x][y].setName("");
                 gridButtons[x][y].setEnabled(false);
@@ -302,10 +365,10 @@ public class GUI implements ActionListener {
     	
     	
     }
-	
-	// Selects the square you want to go
-    public void actionPerformed(ActionEvent a) {
-    	// Draws the letter on the button, disable the button and enables adjacent.
+  //Selects the square you want to go
+    public void actionPerformed(ActionEvent a) 
+    {
+    	//Draws the letter on the button, disable the button and enables adjacent.
     	
         JButton pressedButton = (JButton)a.getSource();
         pressedButton.setText("U");
@@ -313,11 +376,15 @@ public class GUI implements ActionListener {
         pressedButton.setEnabled(false);
         int tempArray[] = new int[2];
         
-        for(int x = 0; x < grid - 1; x++) {
-        	for(int y = 0; y < grid - 1; y++) {
-        		if (gridButtons[x][y] == a.getSource()) {        			
+        for(int x = 0; x < grid - 1; x++)
+        {
+        	for(int y = 0; y < grid - 1; y++)
+        	{
+        		if (gridButtons[x][y] == a.getSource())
+        		{        			
         			
-        			if (x == 0 || y == 0) {
+        			if (x == 0 || y == 0)
+        			{
         				tempArray = shift(x, y);
         				x = tempArray[0];
         				y = tempArray[1];
@@ -328,11 +395,15 @@ public class GUI implements ActionListener {
         		}
         	}
         }
-        for(int x = grid - 1; x > 1; x--) {
-        	for(int y = grid - 1; y > 1; y--) {
-        		if (gridButtons[x][y] == a.getSource()) {        			
+        for(int x = grid - 1; x > 1; x--)
+        {
+        	for(int y = grid - 1; y > 1; y--)
+        	{
+        		if (gridButtons[x][y] == a.getSource())
+        		{        			
         			
-        			if (x == grid - 1 || y == grid - 1) {
+        			if (x == grid - 1 || y == grid - 1)
+        			{
         				tempArray = shift(x, y);
         				x = tempArray[0];
         				y = tempArray[1];
@@ -346,29 +417,35 @@ public class GUI implements ActionListener {
         
     }
    
-	// Redundant: Shifts the board when necessary
-    private int[] shift(int x, int y) {
+    private int[] shift(int x, int y) 
+    {
     	int flag;
     	int j, k;
     	JButton tempGrid[][] = new JButton[grid][grid];
     	int tempArray[] = new int[2];
     	tempGrid = initBoard(tempGrid);
-    	if (x == 0 || y == 0) {
+    	if (x == 0 || y == 0)
+    	{
     		j = 0;
-	    	if (x == 0) {
+	    	if (x == 0)
+	    	{
 	    		flag = 0;
 	    		x = 1;
 	    	}
-	    	else {
+	    	else
+	    	{
 	    		flag = 1;
 	    		y = 1;
 	    	}
 	    	
-			while(j < grid -1) {
+			while(j < grid -1)
+			{
 				k = 0;
 			
-				while(k < grid - 1) {
-					loopOne: switch (flag) {
+				while(k < grid - 1)
+				{
+					loopOne: switch (flag) 
+					{
 					case 0: tempGrid[j+1][k].setText(gridButtons[j][k].getText());
 							tempGrid[j+1][k].setName(gridButtons[j][k].getName());
 							tempGrid[j+1][k].setEnabled(gridButtons[j][k].isEnabled());
@@ -394,23 +471,29 @@ public class GUI implements ActionListener {
 				j++;
 			}
     	}
-			if (x == grid - 1 || y == grid - 1) {
+			if (x == grid - 1 || y == grid - 1)
+	    	{
 				j = grid - 1;
 	    	
-		    	if (x == grid - 1) {
+		    	if (x == grid - 1)
+		    	{
 		    		flag = 0;
 		    		x = grid - 2;
 		    	}
-		    	else {
+		    	else
+		    	{
 		    		flag = 1;
 		    		y = grid - 2;
 		    	}
 		    	
-		    	while(j > 0) {
+		    	while(j > 0)
+				{
 					k = grid - 1;
 					
-					while(k > 0) {
-						loopTwo: switch (flag) {
+					while(k > 0)
+					{
+						loopTwo: switch (flag) 
+						{
 						case 0: tempGrid[j-1][k].setText(gridButtons[j][k].getText());
 								tempGrid[j-1][k].setName(gridButtons[j][k].getName());
 								tempGrid[j-1][k].setEnabled(gridButtons[j][k].isEnabled());
@@ -436,7 +519,8 @@ public class GUI implements ActionListener {
 					j--;
 				}
 	    	}
-	    	for (int l = 0; l < grid ; l++) {
+	    	for (int l = 0; l < grid ; l++)
+	    	{
 	    		tempGrid[l][0].setText("");
 	    		tempGrid[l][0].setName("");
 	    		tempGrid[l][0].setEnabled(false);
@@ -459,24 +543,31 @@ public class GUI implements ActionListener {
 	    		
 	    	}
     
-    	for(int n = 0; n < grid; n++ ) {
-    		for (int m = 0; m < grid; m++) {
+    	for(int n = 0; n < grid; n++ )
+    	{
+    		for (int m = 0; m < grid; m++)
+    		{
     			
     			gridButtons[n][m].setText(tempGrid[n][m].getText());
 				gridButtons[n][m].setName(tempGrid[n][m].getName());
 				gridButtons[n][m].setEnabled(tempGrid[n][m].isEnabled());
 				gridButtons[n][m].setBackground(tempGrid[n][m].getBackground());
-				if(gridButtons[n][m].getName() == "used") {
-        			if (gridButtons[n-1][m].getName() != "used") {
+				if(gridButtons[n][m].getName() == "used")
+    			{
+        			if (gridButtons[n-1][m].getName() != "used")
+        			{
         				gridButtons[n-1][m].setEnabled(true);
         			}
-        			if (gridButtons[n+1][m].getName() != "used") {
+        			if (gridButtons[n+1][m].getName() != "used")
+        			{
         				gridButtons[n+1][m].setEnabled(true);
         			}
-        			if (gridButtons[n][m-1].getName() != "used") {
+        			if (gridButtons[n][m-1].getName() != "used")
+        			{
         				gridButtons[n][m-1].setEnabled(true);
         			}
-        			if (gridButtons[n][m+1].getName() != "used") {
+        			if (gridButtons[n][m+1].getName() != "used")
+        			{
         				gridButtons[n][m+1].setEnabled(true);
         			}
     			}
@@ -486,30 +577,47 @@ public class GUI implements ActionListener {
     	tempArray[1] = y;
     	return tempArray;
     }
-    
-	// Cloan the grid
-	private JButton[][] initBoard(JButton[][] board) {
-    	for(int j = 0 ; j < grid ; j++) {
-    		for(int k = 0 ; k < grid ; k++) {
+    private JButton[][] initBoard(JButton[][] board)
+    {
+    	for(int j = 0 ; j < grid ; j++)
+    	{
+    		for(int k = 0 ; k < grid ; k++)
+    		{
     			board[j][k] = new JButton();
     		}
     	}
 		return board;
+    	
     }
-    
-	// Redundant: Determine if a spot is a valid spot for a new tile to be placed
-	private void modAdjacent(int x, int y) {
-    	if (gridButtons[x-1][y].getName() != "used") {
+    private void modAdjacent(int x, int y)
+    {
+    	if (gridButtons[x-1][y].getName() != "used")
+		{
 			gridButtons[x-1][y].setEnabled(true);
 		}
-		if (gridButtons[x+1][y].getName() != "used") {
+		if (gridButtons[x+1][y].getName() != "used")
+		{
 			gridButtons[x+1][y].setEnabled(true);
 		}
-		if (gridButtons[x][y-1].getName() != "used") {
+		if (gridButtons[x][y-1].getName() != "used")
+		{
 			gridButtons[x][y-1].setEnabled(true);
 		}
-		if (gridButtons[x][y+1].getName() != "used") {
+		if (gridButtons[x][y+1].getName() != "used")
+		{
 			gridButtons[x][y+1].setEnabled(true);
 		}
     }
+
+
+
+	//Runs the Robber Knights Game
+    public static void main(String[] args) throws IOException
+    {
+       new GUI();
+    }
 }
+
+
+    
+        
